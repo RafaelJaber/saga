f830d5784521758cb07f943042dbcbf24d1afc85
Rafael Jaber <rafael.jaber@hotmail.com>
2024-10-25 10:30:35 -0300

    First Commit

---
+++ sale-service/src/test/java/com/jaber/sale/SaleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaber.sale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaleServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
